ba455c64022994d1c49be40a9fac23737dd092dd
DAGS12 <64145012+DAGS12@users.noreply.github.com>
2020-06-26 15:18:02 +0300

    Add files via upload

---
+++ inc/about.inc.php
@@ -0,0 +1,84 @@
+f<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewpoint" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 		<title>SOLA ART TABLE</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+	</head>
+
+	<body>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<table align = "center" width = "60%" border = "1" cellpadding="35">
+	<tr>
+		<th>№</th>
+		<th>Одежда</th>
+		<th>Материал</th>
+		<th>Цена</th>
+	</tr>
+	<tr>
+		<td><strong>1</strong></td>
+		<td><strong>Футболка</strong></td>
+		<td><strong>100% хлопок</strong></td>
+		<td><strong>1000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>2</strong></td>
+		<td><strong>Штаны</strong></td>
+		<td><strong>Лён</strong></td>
+		<td><strong>3000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>3</strong></td>
+		<td><strong>Джинсы</strong></td>
+		<td><strong>Хлопчатобумажная ткань</strong></td>
+		<td><strong>2000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>4</strong></td>
+		<td><strong>Худи</strong></td>
+		<td><strong>Флис</strong></td>
+		<td><strong>1500р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>5</strong></td>
+		<td><strong>Лонгслив</strong></td>
+		<td><strong>Хлопчатобумажный трикотаж</strong></td>
+		<td><strong>1000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>6</strong></td>
+		<td><strong>Пальто</strong></td>
+		<td><strong>Пальтовая ткань</strong></td>
+		<td><strong>5000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>7</strong></td>
+		<td><strong>Arm Sleeves</strong></td>
+		<td><strong>Компрессионный трикотаж</strong></td>
+		<td><strong>1000р</strong></td>
+	</tr>
+	</table>
+			</div>
+		</div>	
+	</div>
+		
+			<footer class="page-footer font-small unique-color-dark pt-0">
+			<div class="primary-color">
+				<div class="container">
+					<div class="row py-4 d-flex align-items-center">
+						<div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+							<h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</footer>	
+
+	</body>
